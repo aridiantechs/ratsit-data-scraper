40291cca152022c5dd512cb4e3ade19bd7f8b0c1
WAHHAB <daniyaldottariq@gmail.com>
2022-05-10 20:09:33 +0500

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '84fc40f0f35c77c4b763aca28b82426a709942c2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '84fc40f0f35c77c4b763aca28b82426a709942c2',
+            'dev_requirement' => false,
+        ),
+        'rct567/dom-query' => array(
+            'pretty_version' => 'v0.8',
+            'version' => '0.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rct567/dom-query',
+            'aliases' => array(),
+            'reference' => '15208bd61f0b87d3befa795a38e5b47a3dbebb97',
+            'dev_requirement' => false,
+        ),
+    ),
+);
